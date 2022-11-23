2faa351732085487fa4a23ad0a345853dbd6338c
prince475 <danielmwai475@gmail.com>
2022-11-23 16:04:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
 
 // Write your code here
+/*let multiply;
+    const num1 = 31;
+    const num2 = 2;
+    multiply = (num1*num2);
+    console.log(multiply)*/
+
+
+        const num1 = 31;
+        const num2 = 2;
+        let multiply = num1 * num2;
+        console.log(multiply)
+        
+        
+                            
+         let random = Math.floor(Math.random() * 10)+1
+            console.log(random)
+        
+        const num3 = 9;
+        const num4 = 5;
+        let mod = num3%num4;
+        console.log(mod)
+        
+        let max = Math.max(0,5,10,15,20);
+        console.log(random)
+
+        
+    
+    
+  
+
